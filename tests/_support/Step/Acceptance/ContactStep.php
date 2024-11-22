08758e2b3de837746bfeded23a0206f60b38a3bd
Anderson, from Dropsolid <116097999+andersonjeccel@users.noreply.github.com>
2024-11-22 14:45:56 -0300

    e2e

--- tests/_support/Step/Acceptance/ContactStep.php
+++ tests/_support/Step/Acceptance/ContactStep.php
@@ -89,8 +89,8 @@ class ContactStep extends \AcceptanceTester
     public function selectOptionFromDropDownContactsPage($option): void
     {
         $I = $this;
-        $I->click('#page-list-actions');
-        $I->click("//ul[contains(@class, 'dropdown-menu')]//li[$option]//span");
+        $I->click('//*[@id="page-list-actions"]');
+        $I->click('//*[@id="page-list-wrapper"]/div[1]/div/div[2]/ul/li[$option]/a');
     }
 
     /**
